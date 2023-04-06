3bf7f5814f28052d017efc8e175d0b71a94342e2
lithrel <lithrel@randomdomainname.net>
2023-04-06 20:02:44 +0200

    Elastic container and commands
    Elastic search container and commands elastic:activate elastic:deactivate
    Shell commands shell:php shell:mysql

---
+++ scripts/elastic.js
@@ -0,0 +1,20 @@
+const { run } = require('./lib/run')
+
+const action = process.argv[2] || 'enable'
+const dcConfig = '-f $(wp-env install-path)/docker-compose.yml -f scripts/docker-compose.p4.yml'
+
+if (action === 'activate') {
+  run(`docker compose ${dcConfig} up -d elasticsearch`)
+  run('wp-env run cli option update ep_host "http://elasticsearch:9200/"')
+  run('wp-env run cli plugin activate elasticpress')
+  run('wp-env run cli elasticpress index --setup --quiet --url=localhost')
+
+  // clear redis cache ?
+  run('wp-env run cli timber clear_cache &>/dev/null')
+  run('wp-env run cli cache flush')
+}
+
+if (action === 'deactivate') {
+  run(`docker compose ${dcConfig} stop elasticsearch`)
+  run('wp-env run cli plugin deactivate elasticpress')
+}
---
+++ scripts/lib/network.js
@@ -0,0 +1,13 @@
+const { run } = require('./run')
+
+function getCurrentNetwork () {
+  const network = JSON.parse(String.fromCharCode(
+    ...run('docker network ls --format=json --filter name=$(basename $(wp-env install-path))', { stdio: 'pipe' })
+  ).trim())
+
+  console.log(network)
+
+  return network.Name || null
+}
+
+module.exports = { getCurrentNetwork }
