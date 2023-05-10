141caab180ea288cd39a392a71de258a9d1845fd
Florent Hernandez <florent.hernandez@greenpeace.org>
2023-05-10 13:52:35 +0200

    Run E2E tests (#13)

--- scripts/elastic.js
+++ scripts/elastic.js
@@ -1,21 +1,29 @@
 const { run } = require('./lib/run')
+const { parseArgs } = require('./lib/utils')
 
-const action = process.argv[2] || 'activate'
+/**
+ * Main
+ */
+const args = parseArgs(process.argv, { command: 'activate' })
 const dcConfig = '-f $(wp-env install-path)/docker-compose.yml -f scripts/docker-compose.p4.yml'
 
-if (action === 'activate') {
-  run(`docker compose ${dcConfig} up -d elasticsearch`)
+if (args.command === 'activate') {
+  run(`docker compose ${dcConfig} up --force-recreate -d elasticsearch`)
   run('wp-env run cli option update ep_host "http://elasticsearch:9200/"')
   run('wp-env run cli plugin activate elasticpress')
-  run('wp-env run cli elasticpress index --setup --quiet --url=localhost')
+  run('wp-env run cli elasticpress index --setup --yes')
 
   // clear redis cache ?
   run('wp-env run cli timber clear_cache &>/dev/null')
   run('wp-env run cli cache flush')
 }
 
-if (action === 'deactivate') {
+if (args.command === 'deactivate') {
+  run(`docker compose ${dcConfig} stop elasticsearch`)
+  run('wp-env run cli plugin deactivate elasticpress')
+}
+
+if (args.command === 'stop') {
   run(`docker compose ${dcConfig} stop elasticsearch`)
   run('wp-env run cli option delete ep_host')
-  // run('wp-env run cli plugin deactivate elasticpress')
 }
