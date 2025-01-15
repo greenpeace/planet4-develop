f647bdd5f254561bf312956428bd98864abc62cb
Nikos Roussos <nikos.roussos@greenpeace.org>
2025-01-15 12:04:38 +0200

    Move usage script inside scripts folder

--- scripts/usage.js
+++ scripts/usage.js
@@ -15,18 +15,24 @@ const usage = `
     env:status                        Status of docker containers
     env:e2e-install                   Install E2E tests dependencies
     env:e2e                           Run E2E tests on local instance
+
     nro:install <?nro>                Install NRO theme and database
     nro:enable                        Enable installed NRO theme and database
     nro:disable                       Switch back to default theme and database
     nro:theme <?nro>                  Clone NRO theme in themes dir
+
     build:assets                      Build main repos assets
     build:repos                       Clone and install main repos
+
     db:import <dump path> <db name>   Import database dump (gzip)
     db:use <db name>                  Switch to database
+
     shell:php                         Access PHP shell (WordPress container)
     shell:mysql                       Access MySQL console (current database)
+
     elastic:activate                  Activate ElasticSearch container and plugin
     elastic:deactivate                Deactivate ElasticSearch container and plugin
+
     xdebug:install                    Install Xdebug in WordPress container
     xdebug:configure                  Rewrite Xdebug configuration and reload server
     xdebug:flamegraph <trace>         Generate a flamegraph from a Xdebug trace file
